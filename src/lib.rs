e213f0d665a6f22fc474596fb3aeca9693e3fe82
Juha Lepola <juha.lepola@gmail.com>
2017-11-18 11:26:54 +0200

    Updated style with rustfmt

--- src/lib.rs
+++ src/lib.rs
@@ -12,93 +12,103 @@ mod tests {
     use ro_scalar_set;
 
     #[test]
-    fn empty_set_is_empty_i32() {
-        
+    fn empty_set_is_empty_i32()
+    {
+
         // Run the test.
         empty_set_is_empty::<i32>();
     }
 
     #[test]
-    fn set_member_is_found_i32() {
-        
+    fn set_member_is_found_i32()
+    {
+
         // Prepare the test.
         let values: [i32; 1] = [5];
-        let values_in_set = [ 5 ];
-        let negative_test = [ 4, 6 ];
+        let values_in_set = [5];
+        let negative_test = [4, 6];
 
         // Run the test.
         set_member_is_found( &values, &values_in_set, &negative_test );
     }
 
     #[test]
-    fn members_from_different_buckets_are_found_i32() {
+    fn members_from_different_buckets_are_found_i32()
+    {
 
         // Prepare test set.
-        let values: Vec<i32> = (0..25).collect();
-        let negative_test = [ 26, 27 ];
+        let values: Vec<i32> = ( 0..25 ).collect();
+        let negative_test = [26, 27];
 
         // Run the test.
         members_from_different_buckets_are_found( &values, 2, &values, &negative_test );
     }
 
     #[test]
-    fn attaching_to_buffer_succeeds_i32() {
+    fn attaching_to_buffer_succeeds_i32()
+    {
 
         // Prepare test set.
-        let buffer = [ 1, 1, 4, 5, 1 ];
-        let values_in_set = [ 1 ];
-        let adjacent_values = [ 0, 2 ];
+        let buffer = [1, 1, 4, 5, 1];
+        let values_in_set = [1];
+        let adjacent_values = [0, 2];
 
         // Run the test.
         attaching_to_buffer_succeeds( &buffer, &values_in_set, &adjacent_values );
     }
 
     #[test]
-    fn empty_set_is_empty_f32() {
+    fn empty_set_is_empty_f32()
+    {
 
         // Run the test.
         empty_set_is_empty::<f32>();
     }
 
     #[test]
-    fn set_member_is_found_f32() {
+    fn set_member_is_found_f32()
+    {
 
         // Prepare the test.
         let values: [f32; 1] = [5.0];
-        let values_in_set = [ 5.0 ];
-        let negative_test = [ 4.0, 6.0 ];
+        let values_in_set = [5.0];
+        let negative_test = [4.0, 6.0];
 
         // Run the test.
         set_member_is_found( &values, &values_in_set, &negative_test );
     }
 
     #[test]
-    fn members_from_different_buckets_are_found_f32() {
+    fn members_from_different_buckets_are_found_f32()
+    {
 
         // Prepare test set.
-        let values: Vec<i32> = (0..25).collect();
+        let values: Vec<i32> = ( 0..25 ).collect();
         let values: Vec<f32> = Vec::from_iter( values.into_iter().map( |x| x as f32 ) );
-        let negative_test = [ 26.0, 27.0 ];
+        let negative_test = [26.0, 27.0];
 
         // Run the test.
         members_from_different_buckets_are_found( &values, 2, &values, &negative_test );
     }
 
     #[test]
-    fn attaching_to_buffer_succeeds_f32() {
+    fn attaching_to_buffer_succeeds_f32()
+    {
 
         // Prepare test set.
-        let buffer = [ 1.0, 1.0, 4.0, 5.0, 1.0 ];
-        let values_in_set = [ 1.0 ];
-        let adjacent_values = [ 0.0, 2.0 ];
+        let buffer = [1.0, 1.0, 4.0, 5.0, 1.0];
+        let values_in_set = [1.0];
+        let adjacent_values = [0.0, 2.0];
 
         // Run the test.
         attaching_to_buffer_succeeds( &buffer, &values_in_set, &adjacent_values );
     }
 
     /// Vefiries that empty set is empty.
-    fn empty_set_is_empty< T >()
-        where T: ro_scalar_set::Value + std::clone::Clone {
+    fn empty_set_is_empty<T>()
+    where
+        T: ro_scalar_set::Value + std::clone::Clone,
+    {
 
         let values: Vec<T> = Vec::new();
         let set = ro_scalar_set::RoScalarSet::new( &values );
@@ -110,10 +120,12 @@ mod tests {
         values: &[T],
         values_in_set: &[T],
         negative_test: &[T],
-    ) where T: ro_scalar_set::Value + std::clone::Clone {
+    ) where
+        T: ro_scalar_set::Value + std::clone::Clone,
+    {
 
         // This should be a simple test.
-        assert_eq!( values.len(), 1  );
+        assert_eq!( values.len(), 1 );
 
         let set = ro_scalar_set::RoScalarSet::new( values );
         assert_eq!( set.size(), 1 );
@@ -123,12 +135,14 @@ mod tests {
     }
 
     /// Verifies that finding values from a set with multiple buckets succeeds.
-    fn members_from_different_buckets_are_found< T >(
+    fn members_from_different_buckets_are_found<T>(
         values: &Vec<T>,
         buckets: usize,
         values_in_set: &[T],
         negative_test: &[T],
-    ) where T: ro_scalar_set::Value + std::clone::Clone {
+    ) where
+        T: ro_scalar_set::Value + std::clone::Clone,
+    {
 
         // This set should verify that more than one bucket is used.
         assert!( buckets > 1 );
@@ -142,11 +156,13 @@ mod tests {
     }
 
     /// Checks that attaching to an existing buffer succeeds.
-    fn attaching_to_buffer_succeeds< T >(
+    fn attaching_to_buffer_succeeds<T>(
         buffer: &[T],
         values_in_set: &[T],
         negative_test: &[T],
-    ) where T: ro_scalar_set::Value + std::clone::Clone {
+    ) where
+        T: ro_scalar_set::Value + std::clone::Clone,
+    {
 
         // Attach to a buffer.
         let set = ro_scalar_set::RoScalarSet::attach( &buffer ).unwrap().0;
@@ -156,20 +172,24 @@ mod tests {
     }
 
     /// Asserts the existance of values in a set.
-    fn assert< T >(
-        set: &ro_scalar_set::RoScalarSet< T >,
+    fn assert<T>(
+        set: &ro_scalar_set::RoScalarSet<T>,
         values_in_set: &[T],
         negative_test: &[T],
-    ) where T: ro_scalar_set::Value + std::clone::Clone {
+    ) where
+        T: ro_scalar_set::Value + std::clone::Clone,
+    {
 
         // Ensure the single member in the set is found.
-        for v in values_in_set {
-            assert!( set.contains( v ));
+        for v in values_in_set
+        {
+            assert!( set.contains( v ) );
         }
 
         // Negative test, i.e. test for values that should not be in the set.
-        for nv in negative_test {
-            assert!( ! set.contains( nv ));
+        for nv in negative_test
+        {
+            assert!( !set.contains( nv ) );
         }
     }
 }
--- src/ro_scalar_set.rs
+++ src/ro_scalar_set.rs
@@ -9,13 +9,16 @@ use self::byteorder::WriteBytesExt;
 /// The values must be able to store booking information
 /// for managing the contents of the set. These requirements
 /// are captured in this trait.
-pub trait Value {
-
+pub trait Value
+{
     /// Gets the index of the bucket for this value.
-    fn get_bucket_index( &self, buckets: &Self ) -> usize;
+    fn get_bucket_index(
+        &self,
+        buckets: &Self,
+    ) -> usize;
 
     /// Gets a zero.
-    fn zero() -> Self;    
+    fn zero() -> Self;
 
     /// Gets the integer value as a size.
     fn as_index( &self ) -> usize;
@@ -35,13 +38,13 @@ pub trait Value {
     /// Provides total ordering for elements.
     fn cmp(
         &self,
-        other: &Self
+        other: &Self,
     ) -> std::cmp::Ordering;
 
     /// Serializes the value.
     fn serialize(
         &self,
-        &mut Write
+        &mut Write,
     ) -> Result<(), std::io::Error>;
 }
 
@@ -49,81 +52,127 @@ pub trait Value {
 impl Value for i32
 {
     /// Gets the index of the bucket for this value.
-    fn get_bucket_index( &self, buckets: &i32 ) -> usize {
-        return ( self.clone() as usize ) % ( buckets.clone() as usize ) ;
+    fn get_bucket_index(
+        &self,
+        buckets: &i32,
+    ) -> usize
+    {
+        return ( self.clone() as usize ) % ( buckets.clone() as usize );
     }
 
     /// Gets a zero.
-    fn zero() -> i32 { return 0; }
+    fn zero() -> i32
+    {
+        return 0;
+    }
 
     /// Gets the integer value as a size.
-    fn as_index( &self ) -> usize { return self.clone() as usize; }
+    fn as_index( &self ) -> usize
+    {
+        return self.clone() as usize;
+    }
 
     /// Converts the specified value to a value that can be stored in the container.
-    fn from_bucket_count( bucket_count: &usize ) -> i32 { return bucket_count.clone() as i32; }
+    fn from_bucket_count( bucket_count: &usize ) -> i32
+    {
+        return bucket_count.clone() as i32;
+    }
 
     /// Converts the number of set members into the value type.
-    fn from_member_count( member_count: usize ) -> i32 { return member_count as i32; }
+    fn from_member_count( member_count: usize ) -> i32
+    {
+        return member_count as i32;
+    }
 
     /// Converts the given value to index.
-    fn from_index( index: usize ) -> i32 { return index as i32; }    
+    fn from_index( index: usize ) -> i32
+    {
+        return index as i32;
+    }
 
     /// Decrements the value by one.
-    fn decrement( &self ) -> i32 { return self - 1; }
+    fn decrement( &self ) -> i32
+    {
+        return self - 1;
+    }
 
     /// Provides total ordering for elements.
     fn cmp(
         &self,
-        other: &i32
-    ) -> std::cmp::Ordering {
+        other: &i32,
+    ) -> std::cmp::Ordering
+    {
         std::cmp::Ord::cmp( self, other )
     }
 
     /// Serializes the value.
     fn serialize(
         &self,
-        writer: &mut Write
+        writer: &mut Write,
     ) -> Result<(), std::io::Error>
     {
         writer.write_i32::<NativeEndian>( *self )?;
-        Ok(())
+        Ok( () )
     }
 }
 
 /// Implements Value trait for f32
 /// * The largest permissible value is 2^24.
 /// * Mainly implemented to provide an easy to use interface for sending the structure to GPU.
-#[cfg(any(feature="floats",test))]
+#[cfg(any(feature = "floats", test))]
 impl Value for f32
 {
     /// Gets the index of the bucket for this value.
-    fn get_bucket_index( &self, buckets: &f32 ) -> usize {
-        return ( self.clone() as usize ) % ( buckets.clone() as usize ) ;
+    fn get_bucket_index(
+        &self,
+        buckets: &f32,
+    ) -> usize
+    {
+        return ( self.clone() as usize ) % ( buckets.clone() as usize );
     }
 
     /// Gets a zero.
-    fn zero() -> f32 { return 0.0; }
+    fn zero() -> f32
+    {
+        return 0.0;
+    }
 
     /// Gets the integer value as a size.
-    fn as_index( &self ) -> usize { return self.clone() as usize; }
+    fn as_index( &self ) -> usize
+    {
+        return self.clone() as usize;
+    }
 
     /// Converts the specified value to a value that can be stored in the container.
-    fn from_bucket_count( bucket_count: &usize ) -> f32 { return bucket_count.clone() as f32; }
+    fn from_bucket_count( bucket_count: &usize ) -> f32
+    {
+        return bucket_count.clone() as f32;
+    }
 
     /// Converts the number of set members into the value type.
-    fn from_member_count( member_count: usize ) -> f32 { return member_count as f32; }
+    fn from_member_count( member_count: usize ) -> f32
+    {
+        return member_count as f32;
+    }
 
     /// Converts the given value to index.
-    fn from_index( index: usize ) -> f32 { return index as f32; }
+    fn from_index( index: usize ) -> f32
+    {
+        return index as f32;
+    }
 
     /// Decrements the value by one.
-    fn decrement( &self ) -> f32 { return self - 1.0; }
+    fn decrement( &self ) -> f32
+    {
+        return self - 1.0;
+    }
 
     /// Provides total ordering for elements.
     fn cmp(
         &self,
-        other: &f32
-    ) -> std::cmp::Ordering {
+        other: &f32,
+    ) -> std::cmp::Ordering
+    {
         let me = self.clone() as i32;
         let other = other.clone() as i32;
         std::cmp::Ord::cmp( &me, &other )
@@ -132,11 +181,11 @@ impl Value for f32
     /// Serializes the value.
     fn serialize(
         &self,
-        writer: &mut Write
+        writer: &mut Write,
     ) -> Result<(), std::io::Error>
     {
         writer.write_f32::<NativeEndian>( *self )?;
-        Ok(())
+        Ok( () )
     }
 }
 
@@ -147,98 +196,104 @@ const SIZE_INDEX: usize = 1;
 const FIRST_BUCKET_INDEX: usize = 2;
 
 /// Defines how the data is stored in the scalar set.
- enum Storage<'a, TA, TB> 
-    where TB: 'a {
-
+enum Storage<'a, TA, TB>
+where
+    TB: 'a,
+{
     /// The data is stored as a vector.
-    Vector { data: Vec<TA> },
+    Vector
+    { data: Vec<TA> },
 
     /// The data is stored as a slice.
-    Slice { data: &'a[TB] }
- }
+    Slice
+    { data: &'a [TB] },
+}
 
 /// Scalar set cabable of storing values with Value trait.
 pub struct RoScalarSet<'a, T>
-where T: std::clone::Clone + Value + 'a {
-    _storage: Storage<'a, T, T>
+where
+    T: std::clone::Clone + Value + 'a,
+{
+    _storage: Storage<'a, T, T>,
 }
 
 impl<'a, T> RoScalarSet<'a, T>
-    where T: std::clone::Clone + Value + 'a {
-
+where
+    T: std::clone::Clone + Value + 'a,
+{
     /// Returns a new integer hash set holding the specified values.
     ///
     /// # ArgumentRoScalarSets
     ///
     /// * 'values' Holds the values stored in the hash set.
-    pub fn new<'b, 'c> (
-            values: &'c[T]
-    ) -> RoScalarSet<'b,T> {
-            
+    pub fn new<'b, 'c>( values: &'c [T] ) -> RoScalarSet<'b, T>
+    {
         // Determine the number of buckets. We introduce a 5% overhead.
-        let bucket_count: usize = ( values.len() as f64 * 0.05 ).ceil() as usize;        
-        let mut storage: Vec<T> = vec!( T::zero(); ( FIRST_BUCKET_INDEX + bucket_count + 1 + values.len() ) );
-    
+        let bucket_count: usize = ( values.len() as f64 * 0.05 ).ceil() as usize;
+        let mut storage: Vec<T> = vec![T::zero(); ( FIRST_BUCKET_INDEX + bucket_count + 1 + values.len() )];
+
         // Store the number of buckets.
         let buckets = T::from_bucket_count( &bucket_count );
-        storage[ 0 ] = buckets.clone();
+        storage[0] = buckets.clone();
 
         // Store the number of members.
-        storage[ SIZE_INDEX ] = T::from_member_count( values.len() );
-        
+        storage[SIZE_INDEX] = T::from_member_count( values.len() );
+
         // Count the values required for each bucket.
-        let mut values_in_buckets: Vec<i32> = vec!( 0; bucket_count );
-        for v in values {            
+        let mut values_in_buckets: Vec<i32> = vec![0; bucket_count];
+        for v in values
+        {
             let bucket = v.get_bucket_index( &buckets );
-            values_in_buckets[ bucket ] += 1;
+            values_in_buckets[bucket] += 1;
         }
-        
+
         // Set bucket pointers to point the end of each bucket.
         // They will be decrements one-by-one when the buckets are filled.
         let first_bucket: usize = FIRST_BUCKET_INDEX;
         let data_start: usize = FIRST_BUCKET_INDEX + bucket_count + 1;
         let mut previous_bucket_end = data_start;
-        for b in 0..bucket_count  {
-
+        for b in 0..bucket_count
+        {
             // The end of the previous bucket is the start of the previous bucket.
-            let index = previous_bucket_end + values_in_buckets[ b ].as_index();
-            storage[ b + first_bucket ] = T::from_index( index );
+            let index = previous_bucket_end + values_in_buckets[b].as_index();
+            storage[b + first_bucket] = T::from_index( index );
             previous_bucket_end = index;
         }
 
         // Fix the end of the last bucket.
-        storage[ first_bucket + bucket_count ] = T::from_index( storage.len() );
-        
+        storage[first_bucket + bucket_count] = T::from_index( storage.len() );
+
         // Put values into buckets.
-        for v in values {
-            
+        for v in values
+        {
             // Determine bucket for the value.
             let bucket_id = v.get_bucket_index( &buckets );
-            
+
             // Make room for the new value.
-            storage[ bucket_id + first_bucket ] = storage[ bucket_id + first_bucket ].decrement();
-            let value_index: usize = storage[ bucket_id + first_bucket ].as_index();
-            storage[ value_index ] = v.clone();
+            storage[bucket_id + first_bucket] = storage[bucket_id + first_bucket].decrement();
+            let value_index: usize = storage[bucket_id + first_bucket].as_index();
+            storage[value_index] = v.clone();
         }
-        
+
         // Sort each bucket to enable binary search.
-        for b in 0..bucket_count  {
-        
+        for b in 0..bucket_count
+        {
             // Determine the location of the bucket.
-            let begin: usize = storage[ b + first_bucket ].as_index();
-            let end: usize = storage[ b + first_bucket + 1 ].as_index();
-            if end < begin {
+            let begin: usize = storage[b + first_bucket].as_index();
+            let end: usize = storage[b + first_bucket + 1].as_index();
+            if end < begin
+            {
                 panic!( "Invalid bucket: {}", b );
             }
-            
+
             // Get a splice for sorting.
-            let ( _, remainder ) =  storage.split_at_mut( begin );
-            let ( bucket,  _ )  = remainder.split_at_mut( end - begin );
+            let ( _, remainder ) = storage.split_at_mut( begin );
+            let ( bucket, _ ) = remainder.split_at_mut( end - begin );
             bucket.sort_by( |a, b| a.cmp( b ) );
-        }        
+        }
 
         let storage: Storage<'b, T, T> = Storage::Vector { data: storage };
-        return RoScalarSet { _storage: storage  };
+        return RoScalarSet { _storage: storage };
     }
 
     /// Attaches an integer hash set to a slice holding the values.
@@ -246,12 +301,11 @@ impl<'a, T> RoScalarSet<'a, T>
     /// # ArgumentRoScalarSets
     ///
     /// * 'values' Holds the values stored in the hash set.
-    pub fn attach<'b> (
-            buffer: &'b[T]
-    ) -> Result< ( RoScalarSet<'b, T>, &'b[T] ), &str > {
-
+    pub fn attach<'b>( buffer: &'b [T] ) -> Result<( RoScalarSet<'b, T>, &'b [T] ), &str>
+    {
         // Determine the length of this scalar set.
-        if buffer.len() < 4 {
+        if buffer.len() < 4
+        {
             return Err( "The buffer is to small" );
         }
         let buckets = buffer[0].as_index();
@@ -261,82 +315,85 @@ impl<'a, T> RoScalarSet<'a, T>
         // Attach.
         let ( set, remainder ) = buffer.split_at( total_size );
         let storage: Storage<T, T> = Storage::Slice { data: set };
-        return Ok( ( RoScalarSet { _storage: storage  }, remainder ) );
+        return Ok( ( RoScalarSet { _storage: storage }, remainder ) );
+    }
     }
 
     /// Checks whether the given value exists in the set or not.
     pub fn contains(
         &self,
-        value: &T
-    ) -> bool {
-        
+        value: &T,
+    ) -> bool
+    {
         // Get the bucket associated with the value.
         let bucket = self.get_bucket( &value );
-        
+
         // Locate the value.
-        match bucket.binary_search_by( |probe| probe.cmp( &value ) ) {
+        match bucket.binary_search_by( |probe| probe.cmp( &value ) )
+        {
             Ok( _ ) => return true,
-            Err(_) => return false,
-        }        
+            Err( _ ) => return false,
+        }
     }
 
     /// Gets the number of members in the set.
-    pub fn size(
-        &self
-    ) -> usize {
+    pub fn size( &self ) -> usize
+    {
         let storage = self.borrow_storage();
-        return storage[ SIZE_INDEX ].as_index();
+        return storage[SIZE_INDEX].as_index();
     }
 
     /// Gets the number of buckets.
-    pub fn bucket_count(
-        &self
-    ) -> usize {
+    pub fn bucket_count( &self ) -> usize
+    {
         let storage = self.borrow_storage();
-        return storage[ 0 ].as_index();
+        return storage[0].as_index();
     }
 
     /// Serializes the scalar set into the given writer.
     pub fn serialize<W>(
         &self,
-        writer: &mut W
+        writer: &mut W,
     ) -> Result<(), std::io::Error>
-    where W: Write {
-
+    where
+        W: Write,
+    {
         // Write our intern
         let storage = self.borrow_storage();
-        for v in storage {
+        for v in storage
+        {
             v.serialize( writer )?;
         }
-        Ok(())
+        Ok( () )
     }
-    
+
     /// Gets a read-only slice containing the values of a bucket.
     fn get_bucket(
         &self,
-        value: &T
-    ) -> &[T] {
-        
+        value: &T,
+    ) -> &[T]
+    {
         // Determine the bucket.
         let storage = self.borrow_storage();
-        let bucket_id: usize = value.get_bucket_index( &storage[ 0 ] );
+        let bucket_id: usize = value.get_bucket_index( &storage[0] );
         let bucket_index = bucket_id + FIRST_BUCKET_INDEX;
-        let begin: usize = storage[ bucket_index ].as_index();
-        let end: usize = storage[ ( bucket_index + 1 ) ].as_index();        
-        let  ( _, remainder ) = storage.split_at( begin );
-        let  ( bucket, _ )  = remainder.split_at( end - begin );
-        return bucket;        
+        let begin: usize = storage[bucket_index].as_index();
+        let end: usize = storage[( bucket_index + 1 )].as_index();
+        let ( _, remainder ) = storage.split_at( begin );
+        let ( bucket, _ ) = remainder.split_at( end - begin );
+        return bucket;
+    }
+
     }
 
 
     /// Borrows the storage for accessing the values.
-    fn borrow_storage(
-        &'a self
-    ) -> &'a [T] {
-        let s: &'a [T] = match &self._storage {
+    fn borrow_storage( &'a self ) -> &'a [T]
+    {
+        let s: &'a [T] = match &self._storage
+        {
             &Storage::Vector { ref data } => data.as_slice(),
             &Storage::Slice { ref data } => data,
-       
         };
         return s;
     }
