449dc7271b3606b164f23d3a7283a0f19f6c09a5
Juha Lepola <juha.lepola@gmail.com>
2018-11-06 20:11:01 +0200

    Fixed a warning

--- src/ro_scalar_set.rs
+++ src/ro_scalar_set.rs
@@ -398,7 +398,7 @@ where
     {
         // Determine the number of buckets. We introduce a 5% overhead.
         let bucket_count: usize = ( values.len() as f64 * 0.05 ).ceil() as usize;
-        let mut storage: Vec<T> = vec![T::zero(); ( FIRST_BUCKET_INDEX + bucket_count + 1 + values.len() )];
+        let mut storage: Vec<T> = vec![T::zero(); FIRST_BUCKET_INDEX + bucket_count + 1 + values.len()];
 
         // Store the number of buckets.
         let buckets = T::from_bucket_count( &bucket_count );
